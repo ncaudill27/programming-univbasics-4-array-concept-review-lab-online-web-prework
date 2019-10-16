0d4a15e9a46153918be731462c8a428a0e5ac7cf
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-16 11:27:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,12 +6,9 @@ def find_max_value(array)
   number_check = 0
   length = array.length
   length.times { |index|
-    if array[index] > number_check
+    while array[index] > number_check do
       number_check = array[index]
-      puts array[index]
-    else
     end
-    number_check
   }
 end
 
