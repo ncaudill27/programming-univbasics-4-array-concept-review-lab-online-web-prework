8c7e18be58d09741b69893b320b2ccf0a643bd8b
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-16 11:45:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,6 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Stores value of first element in array
   number_check = array[0]
   length = array.length
   length.times { |index|
