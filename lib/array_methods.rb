9b440c154e020350f912f35d50d884555a303035
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-16 11:51:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,25 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  number_check = array[0]   # Stores value of first element in array.
+  length = array.length 
+  length.times { |index|  # Limits loop to length of array.
+    while array[index] > number_check do  # Compares elements in array, with first one stored.
+      number_check = array[index]   # Stores element if greather than number already stored.
+    end
+  }
+  number_check
 end
 
 def find_min_value(array)
-  # Add your solution here
+  number_check = array[0]
+  length = array.length
+  length.times { |index|
+    while array[index] < number_check do
+      number_check = array[index]
+    end
+  }
+  number_check
 end
