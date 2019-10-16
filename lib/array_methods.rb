40aff83e0785e9a96d06e9e7ef31a0b091ce02d4
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-16 11:25:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,18 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  number_check = 0
+  length = array.length
+  length.times { |index|
+    if array[index] > number_check
+      number_check = array[index]
+      puts array[index]
+    else
+    end
+    number_check
+  }
 end
 
 def find_min_value(array)
